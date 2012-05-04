4d0372c42d601bfc6306908a1de6f7fcd112b782
Simon Josi <simon.josi@atizo.com>
2012-05-04 16:42:22 +0200

    inital commit

---
+++ manifests/git.pp
@@ -0,0 +1,19 @@
+class jenkins::git {
+  include git
+  Class['jenkins::git'] <- Class['jenkins']
+  file{"${jenkins::home}/.ssh":
+    ensure => directory,
+    owner => jenkins, group => jenkins, mode => 0700;
+  }
+  exec{'jenkins_ssh_keypair':
+    user => $jenkins::user,
+    command => "ssh-keygen -f ${jenkins::home}/.ssh/id_rsa -t rsa -N ''",
+    require => File["${jenkins::home}/.ssh"],
+    creates => "${jenkins::home}/.ssh/id_rsa",
+  }
+  file{"${jenkins::home}/.ssh/config":
+    content => 'StrictHostKeyChecking no',
+    require => File["${jenkins::home}/.ssh"],
+    owner => jenkins, group => jenkins, mode => 0444;
+  }
+}
---
+++ manifests/init.pp
@@ -0,0 +1,55 @@
+#
+# jenkins module
+#
+# Copyright 2010, Atizo AG
+# Simon Josi simon.josi+puppet(at)atizo.com
+#
+# This program is free software; you can redistribute 
+# it and/or modify it under the terms of the GNU 
+# General Public License version 3 as published by 
+# the Free Software Foundation.
+#
+
+class jenkins(
+  $home = '/var/lib/jenkins',
+  $java_cmd = '',
+  $user = 'jenkins',
+  $java_options = '-Djava.awt.headless=true',
+  $port = '8080',
+  $ajp_port = '8009',
+  $debug_level = 5,
+  $enable_access_log = 'no',
+  $handler_max = 100,
+  $handler_idle = 20,
+  $extra_arguments = '',
+  $git_support = false
+) {
+  yum::repo{'jenkins':
+    descr => 'Jenkins Repository',
+    baseurl => 'http://pkg.jenkins-ci.org/redhat',
+    enabled => 1,
+    gpgcheck => 0,
+  }
+  package{'jenkins':
+    ensure => present,
+    require => Yum::Repo['jenkins'],
+  }
+  service{'jenkins':
+    ensure => running,
+    enable => true,
+    hasstatus => true,
+    require => [
+      Package['jenkins'],
+      File['/etc/sysconfig/jenkins'],
+    ],
+  }
+  file{'/etc/sysconfig/jenkins':
+    content => template('jenkins/sysconfig.erb'),
+    require => Package['jenkins'],
+    notify => Service['jenkins'],
+    owner => root, group => root, mode => 0444;
+  }
+  if $git_support {
+    include jenkins::git
+  }
+}
